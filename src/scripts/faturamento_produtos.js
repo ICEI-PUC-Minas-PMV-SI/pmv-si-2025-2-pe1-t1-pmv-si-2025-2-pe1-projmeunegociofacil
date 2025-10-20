be7b7e70710d012b158a1888e02a6b160e569e0f
Lucas <lucasmef@gmail.com>
2025-10-20 18:51:30 -0300

    feat: login

---
+++ src/scripts/faturamento_produtos.js
@@ -0,0 +1,14 @@
+var client
+
+function searchClient () {
+    if (client==null) {
+        console.log('consumidor final');
+    }
+   
+}
+function searchItem () {}
+function addItem () {}
+function save () {}
+function finish () {}
+
+searchClient();
