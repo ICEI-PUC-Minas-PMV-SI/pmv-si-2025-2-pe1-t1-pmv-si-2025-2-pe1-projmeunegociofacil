1599702847cd3057a5e180cbd38d066d768e31a1
Lucas <lucasmef@gmail.com>
2025-11-05 19:08:16 -0300

    implement modules

---
+++ src/assets/js/utils.js
@@ -0,0 +1,27 @@
+const quantitySpan = document.getElementById('quantity');
+const totalSpan = document.getElementById('totalizer-total');
+
+export function makeDecimal(number) {
+    return number.toLocaleString('pt-BR', {
+        style: 'decimal',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })
+
+}
+
+export function totalizerQuantity(itensToSale) {
+    const quantify = itensToSale.reduce((acumulador, produto) => {
+        return acumulador + produto.quantidade;
+    }, 0);
+    quantitySpan.innerHTML = quantify;
+}
+
+export function totalizerTotal(itensToSale) {
+    const valorTotal = itensToSale.reduce((acumulador, produto) => {
+        const subtotalProduto = produto.quantidade * produto.preco_venda;
+        return acumulador + subtotalProduto;
+    }, 0);
+    totalSpan.innerHTML = makeDecimal(valorTotal);
+}
+
